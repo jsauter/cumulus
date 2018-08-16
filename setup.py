26551d9b25e06baef18eaeb999a69d569d488c3a
Brett Swift <Brett.Swift@sjrb.ca>
2018-08-16 08:05:23 -0600

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     tests_require=test_requirements,
     extras_require=extras,
     url='https://github.com/brettswift/cumulus',
-    version='0.1.2',
+    version='0.1.3',
     zip_safe=False,
 )
