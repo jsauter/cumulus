dc44bcec3da4dd233eade3da423885b13d16625e
John Shaskin <guitarjohn@gmail.com>
2018-10-05 11:13:07 -0600

    Bump version: 0.1.6 → 0.1.7

--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ setup(
     tests_require=test_requirements,
     extras_require=extras,
     url='https://github.com/brettswift/cumulus',
-    version='0.1.6',
+    version='0.1.7',
     zip_safe=False,
 )
